33eddc0eafa516cbf8b894eacb9d4dbb323d96d3
Revanth Pobala <revanth.linux@gmail.com>
2016-07-11 13:00:47 -0500

    Updated the License agreement.

--- Directory_Server/public/main.js
+++ Directory_Server/public/main.js
@@ -1,3 +1,25 @@
+/*
+  Copyright (c) <2016> <Hussain Mucklai & Revanth Pobala>
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy of
+  this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be
+  included in all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+  DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+  OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+  OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
 //Chat input validation whitelist
 var display;
 var tagWhitelist_ = {
@@ -1468,8 +1490,7 @@ function initialize() {
         var infoIpAddress = [];
         var geocoder = new google.maps.Geocoder();
         for (var ipAd = 0; ipAd < ipAddress.length; ipAd++) {
-            url[ipAd] = "http://ipinfo.io/" + ipAddress[ipAd];
-
+					url[ipAd] = "http://ipinfo.io/" + ipAddress[ipAd];
             $.ajax({
                 type: "Get",
                 url: url[ipAd],
